b3e1829559d038a7f31c8e8108dbfa99b49ba558
Andres Munoz <andremude@hotmail.com>
2023-05-19 17:40:34 -0500

    add handle unauthorized

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::BooksController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_book, only: %i[show edit update destroy]
 
   def index
+    @books = current_user.books.all
   end
 
   def show
@@ -20,5 +22,15 @@ class Api::V1::BooksController < ApplicationController
 
   def set_book
     @book = Book.find(params[:id])
+  end
+
+  def authorized?
+    @book.user == curent_user
+  end
+
+  def handle_unauthorized
+    unless authorized?
+      respond_to { |format| format.json { render :unauthorized, status: 401 } }
+    end
   end
 end
