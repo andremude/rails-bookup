6780b9fe438292b587267656d28815d6bb72f6b5
Andres Munoz <andremude@hotmail.com>
2023-05-22 21:50:40 -0500

    add delete book

--- app/javascript/packs/components/Book.jsx
+++ app/javascript/packs/components/Book.jsx
@@ -1,7 +1,15 @@
 import React, { useState } from 'react'
+import axios from 'axios';
+import setAxiosHeaders from "./AxiosHeaders.jsx";
 
-const Book = ( {title, author, id}) => {
+const Book = ( {title, author, id, getBooks}) => {
   const [finished, setFinished] = useState()
+
+  const handleDelete = async () => {
+    setAxiosHeaders();
+    await axios.delete(`http://localhost:3000/api/v1/books/${id}`)
+    getBooks()
+  }
 
   return (
     <tr className={`${finished ? "table-light" : ""} text-center`} >
@@ -58,7 +66,7 @@ const Book = ( {title, author, id}) => {
         </div>
       </td>
       <td>
-        <button className="btn btn-outline-danger">
+        <button className="btn btn-outline-danger" onClick={handleDelete}>
           <svg height="20px" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
           </svg>
--- app/javascript/packs/components/Books.jsx
+++ app/javascript/packs/components/Books.jsx
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Book from "./Book.jsx"
+import BookForm from './BookForm.jsx';
 
 const Books = () => {
 
   const [books, setBooks] = useState([])
+  const [isUpdate, setUpdate] = useState(false)
 
   useEffect(() => {
-    getBooks()},
-    [])
+    getBooks()
+    setUpdate(false)},
+    [isUpdate])
 
 
   const getBooks = async () => {
@@ -18,19 +21,27 @@ const Books = () => {
   }
 
 
+  const updateBooks = (Book) => {
+    let book = books;
+    book.unshift(Book);
+    setBooks(books);
+    setUpdate(true)
+  }
+
   return (
-    <div className="table-responsive">
+    <div className="table-responsive px-2">
+      <BookForm updateBooks={updateBooks}/>
       <table className="table">
         <thead>
-          <tr>
-            <th className="col text-center">Status</th>
-            <th className="col text-center">Title</th>
-            <th className="col text-center">Author</th>
-            <th className="col"></th>
-            <th className="col"></th>
-            </tr>
+          <tr className=''>
+            <th className="text-center">Status</th>
+            <th className="text-center">Title</th>
+            <th className="text-center">Author</th>
+            <th className=""></th>
+            <th className=""></th>
+          </tr>
         </thead>
-        <tbody>
+        <tbody className='mt-4'>
           {books.map((book) => (
           <>
             <Book
@@ -38,6 +49,7 @@ const Books = () => {
               title={book.title}
               author={book.author}
               id={book.id}
+              getBooks={getBooks}
               />
           </>
           ))}
